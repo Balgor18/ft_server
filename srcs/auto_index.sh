27f24d05029a7b4d112b3f5f64db3c6d5a2f01ae
Florian CATINAUD <fcatinau@e2r9p10.42.fr>
2021-03-08 10:28:02 +0100

    work

--- srcs/auto_index.sh
+++ srcs/auto_index.sh
@@ -1,6 +1,6 @@
 if ((ps -ef | grep -v grep | grep nginx | wc -l) > 0 )
 	then if [ "$AUTOINDEX" = "off" ]
-		then cp /tmp/server_auto_off.conf /etc/nginx/sites-available/default;
+		then cp /tmp/serv_auto_off.conf /etc/nginx/sites-available/default;
 	else
 		cp /tmp/nginx.conf /etc/nginx/sites-available/default;
 	fi
